dad8a98d31c6fccf3a97e1892f5d3f011f717ae2
Roma <rj.ten.april@gmail.com>
2021-03-19 19:50:09 +0200

    first commit

---
+++ src/main/java/com/test/intelliartstestapp/IntelliartstestappApplication.java
@@ -0,0 +1,13 @@
+package com.test.intelliartstestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliartstestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliartstestappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/intelliartstestapp/IntelliartstestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.intelliartstestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliartstestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
